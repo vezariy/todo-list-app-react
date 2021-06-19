76f4082fc7e7787fdee51c4d45727b61bb7cf7d9
vezaliy <vezaliyy@gmail.com>
2021-06-19 23:31:24 +0400

    #4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 // Components
 import Form from './components/Form';
@@ -6,8 +6,38 @@ import TodoList from './components/TodoList';
 // import Todo from './components/Todo';
 
 function App() {
+//  States
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+// useEffet
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+  
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default: 
+        setFilteredTodos(todos);
+        break;
+      }
+  }
+  // Save to local
+  // const saveLocalTodos = () => {
+  //   if (localStorage.getItem('todos') === null) {
+  //     localStorage.setItem('todos', JSON.stringify([]));
+  //   } else {
+  //     localStorage.setItem('todos', JSON.stringify(todos));
+  //   }
+  
+
   return (
     <div className="App">
       <header>
@@ -18,9 +48,14 @@ function App() {
       todos={todos} 
       setTodos={setTodos} 
       setInputText = {setInputText}
+      setStatus={setStatus}
       />
       
-      <TodoList setTodos={setTodos} todos={todos}/>
+      <TodoList 
+      setTodos={setTodos} 
+      todos={todos}
+      filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
